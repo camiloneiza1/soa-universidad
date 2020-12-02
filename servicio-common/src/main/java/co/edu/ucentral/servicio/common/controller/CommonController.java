8ae55cc4b6dd07dbc8189bcd5dac7234e3e9b379
davidsval11 <davidsval@hotmail.com>
2020-12-02 09:52:54 -0600

    Actualización controlador Common

--- servicio-common/src/main/java/co/edu/ucentral/servicio/common/controller/CommonController.java
+++ servicio-common/src/main/java/co/edu/ucentral/servicio/common/controller/CommonController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import co.edu.ucentral.servicio.common.service.CommonService;
 
 
+@CrossOrigin("http://localhost:4200")
 public class CommonController<E,S extends CommonService<E>> {
 
 	@Autowired
